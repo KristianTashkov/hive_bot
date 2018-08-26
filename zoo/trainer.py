c67f37b33025ea02654d2f7d69ced68d46c93003
Kristian Tashkov <kristian.tashkov@gmail.com>
2018-08-26 14:57:54 +0300

    better reward

--- zoo/trainer.py
+++ zoo/trainer.py
@@ -2,6 +2,8 @@ import os
 import sys
 
 import numpy as np
+
+from engine.game_piece import GamePieceType
 from engine.hive_game import HiveGame
 from zoo.model import Model
 from collections import Counter
@@ -12,23 +14,33 @@ from zoo.players import ModelPlayer
 
 def get_reward(game, for_player):
     winner = game.get_winner()
-    turns_penalty = -game.turns_passed * 0.01
-    if winner is None or winner == -1:
+    turns_penalty = -game.turns_passed * 0.001
+    if winner is None:
+        return turns_penalty
+        #player_queen = game.get_piece(for_player, 0)
+        #opponent_queen = game.get_piece((for_player + 1) % 2, 0)
+        #if None in [player_queen, opponent_queen]:
+        #    return 0
+        #player_taken = len(game.neighbor_pieces(player_queen.position))
+        #opponent_taken = len(game.neighbor_pieces(opponent_queen.position))
+        #return opponent_taken - player_taken + turns_penalty
+    if winner == -1:
         return turns_penalty
     return (10.0 if winner == for_player else -10.0) + turns_penalty
 
 
-gamma = 0.99
-batch_size = 10
+gamma = 0.98
+batch_size = 200
 
 
 def get_discount_rewards(r):
     """ take 1D float array of rewards and compute discounted reward """
     discounted_r = np.zeros_like(r)
-    running_add = 0
-    for t in reversed(range(0, len(r))):
-        running_add = running_add * gamma + r[t]
-        discounted_r[t] = running_add
+    discounted_r[-1] = r[-1]
+    new_reward = r[-1]
+    for t in reversed(range(0, len(r) - 1)):
+        new_reward *= gamma
+        discounted_r[t] = new_reward + r[t]
     return discounted_r
 
 
@@ -42,7 +54,7 @@ def simulate_games(checkpoint=None, opponent_checkpoint=None, exit_win_rate=0.7,
     game = HiveGame(to_win=to_win)
     results = []
     exception_in_last_runs = []
-    experiment_name = experiment_name if experiment_name is not None else round(time.time())
+    experiment_name = experiment_name if experiment_name is not None else str(round(time.time()))
     with Model(game, PLAYER_ID, is_training=True, checkpoint=checkpoint, save_dir=save_dir) as model:
         with ModelPlayer(game, OPPONENT_ID, is_training=False, checkpoint=opponent_checkpoint) as opponent:
             game_index = 0
@@ -56,6 +68,7 @@ def simulate_games(checkpoint=None, opponent_checkpoint=None, exit_win_rate=0.7,
                     rounds_remaining = max_rounds
                     game_history = []
                     discounted_rewards = []
+                    old_reward = 0
                     while game.get_winner() is None and rounds_remaining > 0:
                         state = model.get_state(game)
                         action_id, action = model.choose_action(state)
@@ -64,13 +77,13 @@ def simulate_games(checkpoint=None, opponent_checkpoint=None, exit_win_rate=0.7,
                             opponent.play_move()
 
                         reward = get_reward(game, PLAYER_ID)
-                        game_history.append([state, action_id, reward])
+                        game_history.append([state, action_id, reward - old_reward])
+                        old_reward = reward
                         discounted_rewards = get_discount_rewards([x[2] for x in game_history])
                         for j in range(len(game_history)):
                             game_history[j][2] = discounted_rewards[j]
 
-                            rounds_remaining -= 1
-                    winner = game.get_winner()
+                        rounds_remaining -= 1
                     all_states = np.vstack([x[0]['board'] for x in game_history])
                     all_allowed = np.vstack([x[0]['allowed_actions'] for x in game_history])
                     played_actions = np.array([x[1] for x in game_history])
@@ -79,6 +92,7 @@ def simulate_games(checkpoint=None, opponent_checkpoint=None, exit_win_rate=0.7,
                                                all_allowed[batch_index * batch_size:(batch_index + 1) * batch_size, ...],
                                                played_actions[batch_index * batch_size:(batch_index + 1) * batch_size],
                                                discounted_rewards[batch_index * batch_size:(batch_index + 1) * batch_size])
+                    winner = game.get_winner()
                     results.append(winner if winner is not None else -1)
                     if game_index != 0 and game_index % log_every == 0:
                         print(game_index, Counter(np.array(results)[-log_every:]))
@@ -110,7 +124,7 @@ def full_training(checkpoint=None, save_every=500, log_every=50, to_win_start=4,
     while True:
         experiment_name = "{}_{}".format(main_name, experiment_index)
         last_game_index = simulate_games(checkpoint=checkpoint, opponent_checkpoint=checkpoint,
-                                         save_every=save_every, log_every=log_every, max_rounds=(to_win * 30),
+                                         save_every=save_every, log_every=log_every, max_rounds=50,
                                          to_win=to_win, experiment_name=experiment_name, save_dir=directory)
         checkpoint = os.path.join(directory, experiment_name, 'model.ckpt-' + str(last_game_index))
         experiment_index += 1
